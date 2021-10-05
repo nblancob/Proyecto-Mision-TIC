c29424bef8feb7801d530c16aafaf2f7c8a529a9
nblancob <nblancob@unal.edu.co>
2021-10-05 16:32:17 -0500

    Update managment product

--- my-app/src/home/pages/components/Login.js
+++ my-app/src/home/pages/components/Login.js
@@ -1,20 +1,25 @@
-import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
-const Login =() =>{
-  return(
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Control type="email" placeholder="Ingrese su usuario" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Button  style={{width: "100%"}} variant="primary" type="submit" href="/user/managment">
-          Iniciar
-        </Button>
-      </Form>
-    );
-}
+const Login = () => {
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Control type="email" placeholder="Ingrese su usuario" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button
+        style={{ width: "100%" }}
+        variant="primary"
+        type="submit"
+        href="/user/managment"
+      >
+        Iniciar
+      </Button>
+    </Form>
+  );
+};
 
 export default Login;
