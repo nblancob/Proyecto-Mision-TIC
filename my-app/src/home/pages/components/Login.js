b8aad88a8b7bedbbfe45f781346c89584e1aa993
Jeovan Herrera <jeovanis@outlook.com>
2021-10-04 21:33:10 -0500

    Agregar tabla de control Usuarios

--- my-app/src/home/pages/components/Login.js
+++ my-app/src/home/pages/components/Login.js
@@ -2,30 +2,6 @@ import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 
 const Login =() =>{
-<<<<<<< HEAD
-    return(
-        <div className="container justify-content-space-around">
-          <Form >
-              <Col md="4" className="justify-content-center container">
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label className="title">Usuario</Form.Label>
-    <Form.Control type="text" placeholder="Ingrese su usuario" />
-    <Form.Text className="text-muted">
-    </Form.Text>
-  </Form.Group>
-
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label className="title">Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Button variant="primary" type="submit" href="/user/managment" className="float-end">
-    Iniciar
-  </Button>
-  </Col>
-</Form>
-            
-        </div>
-=======
   return(
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
