59ad8b3170f7142893d75f4b01d4afe30879c644
JuanMiguel <capitanisaza@gmail.com>
2021-10-15 21:33:05 -0500

    Router de Updateone user

--- my-app/backend/routes/users.js
+++ my-app/backend/routes/users.js
@@ -5,5 +5,6 @@ const UserController = require("../controllers/users");
 
 router.get("",UserController.getUsers);
 router.post("",UserController.addUsers);
+router.post("",UserController.updateOneUsers);
 router.delete("",UserController.removeUsers);
 module.exports = router;
