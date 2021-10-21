4d39046b1d0a3191cb9a704eb244d0688f5584e7
nblancob <nblancob@unal.edu.co>
2021-10-20 21:33:53 -0500

    conflicts

--- my-app/backend/app.js
+++ my-app/backend/app.js
@@ -4,6 +4,8 @@ var mongoose = require("mongoose");
 const productsRoute = require("./routes/products");
 const salesRoute = require("./routes/sales");
 const usersRoute = require("./routes/users");
+
+const URL = "mongodb+srv://nblancob:Nico123@cluster0.gxith.mongodb.net/Cluster0?retryWrites=true&w=majority"
 const cors = require("cors");
 
 //Middlewares
--- my-app/src/Api.js
+++ my-app/src/Api.js
@@ -1,4 +1,10 @@
 const CallApi = async (url, options = {}) => {
+  options.headers = {
+    "Content-type": "application/json",
+    Accept: "application/json",
+  };
+
+  const response = await fetch("http://localhost:3000/api" + url, options);
   const data = await response.json();
   return data;
 };
